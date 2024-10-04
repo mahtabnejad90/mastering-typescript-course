05514930219586d924c50a75784394d6b2ee5ad7
Mahtab Bahman Nejad <mahtab@bahmannejad.com>
2024-10-04 18:04:49 +0100

    first-file

---
+++ basics.js
@@ -0,0 +1,7 @@
+// .ts - can't have JSX
+// .tsx - can have JSX
+var pi = 3.14159;
+console.log("Hello!");
+console.log("Goodbye!");
+console.log(Math.round(7.3287468888888));
+// compile via `tsc basics.ts`
---
+++ basics.ts
@@ -0,0 +1,10 @@
+// .ts - can't have JSX
+
+// .tsx - can have JSX
+
+let pi: number = 3.14159;
+console.log("Hello!");
+console.log("Goodbye!");
+console.log(Math.round(7.3287468888888))
+
+// compile via `tsc basics.ts`
